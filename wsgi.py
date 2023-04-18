bb9ed7918a32abbab819ae9c55f586e794181205
Volker Austrup <volker.andreas.austrup@cern.ch>
2023-04-18 15:47:43 +0100

    fix error in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from workspaceexplorer import init_app
 
 
 application = init_app()
-celery_app = flask_app.extensions["celery"]
+celery_app = application.extensions["celery"]
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0")
